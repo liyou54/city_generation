1e896db785c76309ae5395ab5727e3ea148ca9cb
liyou54 <864757331@qq.com>
2021-06-16 22:17:41 +0800

    aaa

--- city/Assets/Scripts/GenerateMesh.cs
+++ city/Assets/Scripts/GenerateMesh.cs
@@ -2,7 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateMesh 
+public class GenerateMesh
 {
-    
+    static private uint now_id;
+
+    private uint SetId()
+    {
+        return ++now_id;
+    }
+
+    public uint ID
+    {
+        get => ID;
+        private set => SetId();
+    }
+
+    GenerateMesh()
+    {
+    }
 }
